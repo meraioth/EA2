8f2fd2a3a3b4ae634a3278222971803c8f42c22b
Meraioth Ulloa <mb.us.94@gmail.com>
2016-10-21 11:54:15 -0300

    main para 1 intento

--- main.cpp
+++ main.cpp
@@ -21,68 +21,162 @@ string get_file_contents(const char *filename)
   if (in)
   {
     return(string((istreambuf_iterator<char>(in)), istreambuf_iterator<char>()));
-  };
+  }
+  else return 0;
+}
+
+string gen_random(const int len) {
+	string str;
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < len; ++i) {
+        str.push_back(alphanum[rand() % (sizeof(alphanum) - 1)]);
+    }
+
+    return str;
+}
+
+
+void randomstr(const char *filename){
+	// pasara archivo text a string
+	string test= get_file_contents(filename);
+	//cout<<test.size()<<endl;
+	int size=test.size();
+	//cout<< int(0.00001*size)<<endl;
+	//tamaño del patron a buscar
+	int pattern_size=30;
+	// posicion del patron a buscar
+	//int patter_pos = rand()%(size - pattern_size);
+	string patron= gen_random(pattern_size);
+	//string patron("GLLLLLLLV");
+	cout << patron<< endl;
+
+	//cout<<patron<<endl;
+	//algoritmo 1
+	BF beff(test);
+	//algoritmo 2
+	BM boyer(test);
+	//algoritmo 3
+	KMPs km(test);
+	//algoritmo 4
+	RK rabit(test);
+	//algoritmo 4*
+	RK2 rk(test);
+	
+	clock_t comienzo;
+	double fin;
+
+
+	
+	vector<int> v1,v2,v3,v4,v4_2 ;
+
+	comienzo=clock();
+	v1=beff.findpattern(patron);
+	fin=(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+	cout <<"Tiempo de fuerza bruta :"<< fin<<endl;
+
+
+	comienzo=clock();
+	v2=boyer.search(patron);
+	fin=(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+	cout <<"Tiempo de boyer moore :"<< fin<<endl;
+
+
+	comienzo=clock();
+	v3=km.KMPSearch(patron);
+	fin=(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+	cout <<"Tiempo de KMP :"<< fin<<endl;
+	v2 = rabit.search(patron);
+
+	comienzo=clock();
+	v4=rabit.search(patron);
+	fin=(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+	cout <<"Tiempo de Rabit-Karp :"<< fin<<endl;
+	
+	comienzo=clock();
+	v4_2=rk.search(patron);
+	fin=(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+	cout <<"Tiempo de Rabit-Karp 2:"<< fin<<endl;
+
+
+}
+
+
+int en_texto(const char *filename){
+	// pasara archivo text a string
+	string test= get_file_contents(filename);
+	//cout<<test.size()<<endl;
+	int size=test.size();
+	//cout<< int(0.00001*size)<<endl;
+	//tamaño del patron a buscar
+	int pattern_size=0.00001*size;
+	// posicion del patron a buscar
+	int patter_pos = rand()%(size - pattern_size);
+	string patron(test.substr(patter_pos,pattern_size));
+	//string patron("GLLLLLLLV");
+
+	//cout<<patron<<endl;
+	//algoritmo 1
+	BF beff(test);
+	//algoritmo 2
+	BM boyer(test);
+	//algoritmo 3
+	KMPs km(test);
+	//algoritmo 4
+	RK rabit(test);
+	//algoritmo 4*
+	RK2 rk(test);
+	
+	clock_t comienzo;
+	double fin;
+
+
+	
+	vector<int> v1,v2,v3,v4,v4_2 ;
+
+	comienzo=clock();
+	v1=beff.findpattern(patron);
+	fin=(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+	cout <<"Tiempo de fuerza bruta :"<< fin<<endl;
+
+
+	comienzo=clock();
+	v2=boyer.search(patron);
+	fin=(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+	cout <<"Tiempo de boyer moore :"<< fin<<endl;
+
+
+	comienzo=clock();
+	v3=km.KMPSearch(patron);
+	fin=(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+	cout <<"Tiempo de KMP :"<< fin<<endl;
+	v2 = rabit.search(patron);
+
+	comienzo=clock();
+	v4=rabit.search(patron);
+	fin=(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+	cout <<"Tiempo de Rabit-Karp :"<< fin<<endl;
+	
+	comienzo=clock();
+	v4_2=rk.search(patron);
+	fin=(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+	cout <<"Tiempo de Rabit-Karp 2:"<< fin<<endl;
+	
+ return 0;
 }
 
 
 int main(){
 srand (time(NULL));
+
+
+randomstr("dna.200MB");
+
+en_texto("dna.200MB");
 //rand() % 10 + 1;
-string test= get_file_contents("proteins200MB");
-//cout<<test.size()<<endl;
-int size=test.size();
-//cout<< int(0.00001*size)<<endl;
-int pattern_size=0.00001*size;
-//string patron(test.substr(10,rand()%pattern_size));
-string patron("GLLLLLLLV");
-
-cout<<patron<<endl;
-
-
-clock_t comienzo;
-comienzo=clock();
-
-
-RK rabit(test);
-vector<int> v2 = rabit.search(patron);
-
-
-cout<<"Largo de apariciones:"<<v2.size()<<"tiempo:"<< (clock()-comienzo)/(double)CLOCKS_PER_SEC <<endl;
-
-
-
-
-comienzo=clock();
-
-
-KMPs km(test);
-vector<int> v1=km.KMPSearch(patron);
-
-
-cout<< "Largo de apariciones:"<<v1.size()<<"tiempo:"<<(clock()-comienzo)/(double)CLOCKS_PER_SEC <<endl;
-
-
-
-
-comienzo=clock();
-
-
-BF beff(test);
-vector<int> v=beff.findpattern(patron);
-
-
-
-cout<<"Largo de apariciones:"<<v.size()<<"tiempo:"<< (clock()-comienzo)/(double)CLOCKS_PER_SEC <<endl;
-
-comienzo=clock();
-
-
-RK2 rk(test);
-vector<int> v3=rk.search(patron);
-
-
-
-cout<< "Largo de apariciones:"<<v3.size()<<"tiempo:"<<(clock()-comienzo)/(double)CLOCKS_PER_SEC <<endl;
 
 
 
@@ -111,8 +205,7 @@ cout<< "Largo de apariciones:"<<v3.size()<<"tiempo:"<<(clock()-comienzo)/(double
 
 // // 	}
 // cout<<endl;
-// BM boyer(text);
-// vector <int> v3 = boyer.search(pattern);
+// 
 // for (int i = 0; i < v3.size(); ++i)
 // {
 // 	cout<<v3[i]<<'\t';
