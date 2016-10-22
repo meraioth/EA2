87483c264d30664e94217fef82d4b0b5d1625c35
Meraioth Ulloa <mb.us.94@gmail.com>
2016-10-21 21:20:08 -0300

    incluido experimento supervisado

--- main_experimento.cpp
+++ main_experimento.cpp
@@ -140,6 +140,79 @@ void en_texto(const char *filename, const char *resultados_archivo){
     fclose(resultados);
 }
 
+void supervisado( const char *resultados_archivo){
+    string test;
+    string patron;
+    int size=100000;
+    int pattern_size;
+
+    for(int i = 0; i<size ; i++)
+        test.push_back('A');
+       
+    
+    test.push_back('B');
+    
+
+    FILE *resultados;
+   
+    char *resultado_buffer;
+    resultados = fopen(resultados_archivo, "w+");
+    for(int pattern_size = 100 ; pattern_size <= 10000 ; pattern_size+=100){
+        for(int i = 0; i<pattern_size ; i++)
+            patron.push_back('A');
+    
+        patron.push_back('B');
+
+        fprintf(resultados,"%d\t",pattern_size);
+
+        double fin1=0.0,fin2=0.0,fin3=0.0,fin4=0.0;
+        for(int i = 0 ; i < 2 ; i ++){
+           
+            //algoritmo 1
+            BF beff(test);
+            //algoritmo 2
+            BM boyer(test,patron);
+            //algoritmo 3
+            KMPs km(test,patron);
+            //algoritmo 4
+            RK rabin(test);
+            //para medir el tiempo de ejecucion
+            clock_t comienzo;
+
+            comienzo=clock();
+            beff.findpattern(patron);
+            fin1=fin1+(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+
+            comienzo=clock();
+            boyer.search(patron);
+            fin2=fin2+(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+
+            comienzo=clock();
+            km.KMPSearch(patron);
+            fin3=fin3+(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+
+            comienzo=clock();
+            rabin.search(patron);
+            fin4=fin4+(clock()-comienzo)/(double)CLOCKS_PER_SEC;
+        }
+        fin1=fin1/10.0;
+        fin2=fin2/10.0;
+        fin3=fin3/10.0;
+        fin4=fin4/10,0;
+        cout<<"1: "<<fin1<<" 2: "<<fin2<<" 3: "<<fin3<<" 4:"<<fin4<<endl;
+        fprintf(resultados,"%lf\t",fin1);
+        fprintf(resultados,"%lf\t",fin2);
+        fprintf(resultados,"%lf\t",fin3);
+        fprintf(resultados,"%lf\n",fin4);
+    }
+    fclose(resultados);
+
+
+
+}
+
+
+
 int main(){
     srand (time(NULL));
     //experimentos sobre texto natural
@@ -157,5 +230,6 @@ int main(){
     //randomstr("dna.100MB","dna100_t.txt");
     //randomstr("dna.200MB","dna200_t.txt");
     //experimento peor caso
+    supervisado("supervisad.txt");
 	return 0;
 }
